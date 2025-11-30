438dd78ede1663d3a9b44d7b2546bc96ba51720a
Luca Ottaviano <lucatremila@gmail.com>
2025-11-30 09:55:54 +0100

    favicon update

--- src/server-actions/get-repo.ts
+++ src/server-actions/get-repo.ts
@@ -1,6 +1,5 @@
 export default async function getRepoContents() {
     try {
-        // const response = await fetch('https://api.github.com/repos/LucaOttvn/DOCS/contents', {
         const response = await fetch('https://api.github.com/repos/LucaOttvn/DOCS/git/trees/main?recursive=1', {
             headers: {
                 'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`,
