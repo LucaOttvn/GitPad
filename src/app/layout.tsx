92b572dc3081fe874b7ce3670c17ae6f9f16fdd9
Luca Ottaviano <lucatremila@gmail.com>
2025-12-06 19:16:48 +0100

    login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import "./globals.css";
-import {ViewTransition} from "react";
+import { ViewTransition } from "react";
 import ToolBar from "../components/toolbar/ToolBar";
-import {ToasterProvider} from "../components/ToasterProvider";
+import { ToasterProvider } from "../components/ToasterProvider";
+import Providers from "../components/Providers";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export const metadata: Metadata = {
   title: "GitPad",
@@ -12,19 +15,23 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions)
+
   return (
     <html lang="en">
       <body>
-        <ViewTransition enter="slide-in" exit="slide-out">
-          {children}
-          <ToolBar />
-          <ToasterProvider />
-        </ViewTransition>
+        <Providers session={session}>
+          <ViewTransition enter="slide-in" exit="slide-out">
+            {children}
+            <ToolBar />
+            <ToasterProvider />
+          </ViewTransition>
+        </Providers>
       </body>
     </html>
   );
