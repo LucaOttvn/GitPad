5c859a1baa91de224ed0e8630f1705080ef3f1a9
Luca Ottaviano <lucatremila@gmail.com>
2025-12-07 16:27:15 +0100

    update home button behavior

--- src/components/toolbar/ExplorerToolBarButtons.tsx
+++ src/components/toolbar/ExplorerToolBarButtons.tsx
@@ -1,36 +1,43 @@
 import Image from "next/image";
 import {useState} from "react";
 import BottomSheet from "../bottom-sheet/BottomSheet";
-import { redirect } from "next/navigation"
-import { PagesEnum } from "@/src/utils/enums";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {PagesEnum} from "@/src/utils/enums";
 
 export default function ExplorerToolBarButtons() {
+  const pathName = usePathname();
+
   const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
 
   const handleBottomSheet = (value: boolean) => {
     setIsBottomSheetOpen(value);
   };
 
+  const home = `/${PagesEnum.fileExplorer}`
+
   return (
     <div className="w-full flex items-center justify-between gap-2">
+      {/* Plus button */}
       <button onClick={() => setIsBottomSheetOpen(true)} className="mainButton clickableItem">
         <span>
           <Image src="/icons/add.svg" alt="folder" width={20} height={20} />
         </span>
       </button>
-      <button className="mainButton clickableItem" onClick={()=> {
-        redirect('/')
-      }}>
+      {/* Home button */}
+      <Link href={`/${PagesEnum.fileExplorer}`} className={`mainButton clickableItem ${pathName === home ? "disabledLink" : ""}`}>
         <span>
           <Image src="/icons/home.svg" alt="folder" width={20} height={20} />
         </span>
-      </button>
+      </Link>
       <div className="flex gap-2">
+        {/* Search button */}
         <button className="mainButton clickableItem" disabled>
           <span>
             <Image src="/icons/search.svg" alt="folder" width={20} height={20} />
           </span>
         </button>
+        {/* Settings button */}
         <button className="mainButton clickableItem" disabled>
           <span>
             <Image src="/icons/settings.svg" alt="folder" width={20} height={20} />
