6f4d2970ec82c4afa8afeea5fe9976e1a3c97b25
Luca Ottaviano <lucatremila@gmail.com>
2025-12-12 00:55:23 +0100

    Wip docs login flow

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,51 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+/**
+ * This file contains the complete NextAuth configuration for GitHub login.
+ * This object defines:
+ * - providers: how users can sign in (here, just GitHub).
+ * - callbacks: control what happens during login/session.
+ * - secret: used to encrypt cookies.
+*/
 export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
+      // clientId: The id that Github uses to identify GitPad
+      // clientSecret: The key that proves the app is legitimate
+      // These two variables are created from the GIthub app's owner (me) from Github (profile > settings > developer settings > OAuth apps) and prove the app is actually owed by me.
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+      // THe "repo" authorization level allows to:
+      // - Read/write code, commits, branches, files
+      // - Manage collaborators, invitations, webhooks
+      // - Access commit statuses, deployments, projects
+      // - Organization resources (teams, projects if user-owned)
       authorization: { params: { scope: "repo" } }
     }),
   ],
   callbacks: {
+    // The jwt callback runs on login, logout and session check (useSession() or getServerSession())
+    // It returns a modified token object that NextAuth then encrypts and stores as a cookie
     async jwt({ token, account }) {
-      // First time user logs in
+      // This condition detects the initial sign-in vs regular session checks
+      // In the first one in fact, the account object looks like this:
+      // { access_token: "gho_...", provider: "github" }, while on subsequent session checks it's undefined
       if (account) {
+        // Add to the standard JWT token the account's access token
         token.accessToken = account.access_token
       }
+      // The returned token will now look like this:
+      // {
+      //   "sub": "github_user_id",
+      //   "name": "userName",
+      //   "email": "userEmail@github.com",
+      //   "picture": "avatar_url",
+      //   "accessToken": "gho_abc123...", (the custom addidion just made)
+      //   "iat": 1734000000,
+      //   "exp": 1734086400
+      // }
+      // This entire object gets encrypted and saved into httpOnly cookies
       return token
     },
     async session({ session, token }) {
