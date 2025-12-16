04fc07a6ecd54727b4f299ccf985a38a78dc1f77
Luca Ottaviano <lucatremila@gmail.com>
2025-12-16 10:19:32 +0100

    test zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+      </head>
       <body>
         <Providers session={session}>
           <ViewTransition enter="slide-in" exit="slide-out">
