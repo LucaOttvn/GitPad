1bd2b54ca4049fcf63e427abaa7dc50786474119
Luca Ottaviano <lucatremila@gmail.com>
2025-12-28 23:26:05 +0100

    breadcrumbs auto scroll added

--- src/components/breadCrumbs/BreadCrumbs.tsx
+++ src/components/breadCrumbs/BreadCrumbs.tsx
@@ -3,20 +3,30 @@ import "./breadCrumbs.scss";
 import {usePathname} from "next/navigation";
 import Link from "next/link";
 import {PagesEnum} from "../../utils/enums";
+import {useEffect, useRef} from "react";
 
 export default function BreadCrumbs() {
   const pathName = usePathname();
   const breadCrumbs = pathName.split("/").filter((x) => x);
   const filteredBreadCrumbs = breadCrumbs.splice(1, breadCrumbs.length - 1);
 
+  const scrollableRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    scrollableRef.current?.scrollTo({
+      left: scrollableRef.current.scrollWidth,
+      behavior: "smooth",
+    });
+  }, [pathName]);
+
   if (filteredBreadCrumbs.length === 0) return null;
 
   // Breadcrumbs don't have to be visible in the login page.
-  if (pathName.includes(`/${PagesEnum.login}`)) return null;
+  if (pathName.includes(`/${PagesEnum.login}`) || pathName.includes(`/${PagesEnum.fileEditor}`)) return null;
 
   return (
     <div className="breadCrumbs">
-      <div className="scrollableContent">
+      <div className="scrollableContent" ref={scrollableRef}>
         {filteredBreadCrumbs.map((breadCrumb, index) => {
           const href = `/${PagesEnum.fileExplorer}/${filteredBreadCrumbs.slice(0, index + 1).join("/")}`;
           return <Link key={breadCrumb + index} href={href} className="breadCrumb">{`/${breadCrumb}`}</Link>;
